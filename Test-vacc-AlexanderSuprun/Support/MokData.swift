7665d2df460247b012efc4962aa0efdbbc390ea8
Allison <suprun-s@bk.ru>
2024-08-18 17:23:54 +0300

    MokData

--- Test-vacc-AlexanderSuprun/Support/MokData.swift
+++ Test-vacc-AlexanderSuprun/Support/MokData.swift
@@ -11,56 +11,56 @@ class MokData {
                 id: 2,
                 title: "Синий",
                 reqired: false,
-                tapped_on_select_all: false
+                tapped_on_select_all: true
             ),
             Category(
                 id: 3,
                 title: "Зелёный",
                 reqired: true,
                 tapped_on_select_all: true
-            ),
-            Category(
-                id: 4,
-                title: "Жёлтый",
-                reqired: false,
-                tapped_on_select_all: false
-            ),
-            Category(
-                id: 5,
-                title: "Фиолетовый",
-                reqired: true,
-                tapped_on_select_all: true
-            ),
-            Category(
-                id: 6,
-                title: "Оранжевый",
-                reqired: false,
-                tapped_on_select_all: true
-            ),
-            Category(
-                id: 7,
-                title: "Розовый",
-                reqired: true,
-                tapped_on_select_all: false
-            ),
-            Category(
-                id: 8,
-                title: "Коричневый",
-                reqired: false,
-                tapped_on_select_all: true
-            ),
-            Category(
-                id: 9,
-                title: "Чёрный",
-                reqired: true,
-                tapped_on_select_all: true
-            ),
-            Category(
-                id: 10,
-                title: "Белый",
-                reqired: false,
-                tapped_on_select_all: false
             )
+//            Category(
+//                id: 4,
+//                title: "Жёлтый",
+//                reqired: false,
+//                tapped_on_select_all: false
+//            ),
+//            Category(
+//                id: 5,
+//                title: "Фиолетовый",
+//                reqired: true,
+//                tapped_on_select_all: true
+//            ),
+//            Category(
+//                id: 6,
+//                title: "Оранжевый",
+//                reqired: false,
+//                tapped_on_select_all: true
+//            ),
+//            Category(
+//                id: 7,
+//                title: "Розовый",
+//                reqired: true,
+//                tapped_on_select_all: false
+//            ),
+//            Category(
+//                id: 8,
+//                title: "Коричневый",
+//                reqired: false,
+//                tapped_on_select_all: true
+//            ),
+//            Category(
+//                id: 9,
+//                title: "Чёрный",
+//                reqired: true,
+//                tapped_on_select_all: true
+//            ),
+//            Category(
+//                id: 10,
+//                title: "Белый",
+//                reqired: false,
+//                tapped_on_select_all: false
+//            )
         ]
     }
 }
