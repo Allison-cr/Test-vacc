3bf2c0746418ceeac6fdea0f24d2d2b8b6583fe7
Allison <suprun-s@bk.ru>
2024-08-18 20:51:33 +0300

    no commit

---
+++ Test-vacc-AlexanderSuprun/Service/DataLoader.swift
@@ -0,0 +1,8 @@
+//
+//  DataLoader.swift
+//  Test-vacc-AlexanderSuprun
+//
+//  Created by Alexander Suprun on 18.08.2024.
+//
+
+import Foundation
---
+++ Test-vacc-AlexanderSuprun/Support/Margins.swift
@@ -0,0 +1,8 @@
+//
+//  Margins.swift
+//  Test-vacc-AlexanderSuprun
+//
+//  Created by Alexander Suprun on 18.08.2024.
+//
+
+import Foundation
