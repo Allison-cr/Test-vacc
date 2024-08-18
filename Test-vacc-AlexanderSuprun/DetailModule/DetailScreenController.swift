f2f443d7e9b6acc5168b7f4d3fc2fb542dbb5802
Allison <suprun-s@bk.ru>
2024-08-18 17:23:08 +0300

    add detail screen

---
+++ Test-vacc-AlexanderSuprun/DetailModule/DetailScreenController.swift
@@ -0,0 +1,26 @@
+//
+//  SecondScreenController.swift
+//  Test-vacc-AlexanderSuprun
+//
+//  Created by Alexander Suprun on 17.08.2024.
+//
+
+import UIKit
+
+final class SecondScreenController: UIViewController {
+    private let viewModel : SecondScreenViewModel
+    
+    init(viewModel: SecondScreenViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
---
+++ Test-vacc-AlexanderSuprun/DetailModule/DetailScreenViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailScreenViewModel.swift
+//  Test-vacc-AlexanderSuprun
+//
+//  Created by Alexander Suprun on 17.08.2024.
+//
+
+import Foundation
+
+final class DetailScreenViewModel {
+    
+}
